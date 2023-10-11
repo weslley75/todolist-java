d0649f63980a5e4791074b6cd06209a7a85ef31f
Weslley Almeida <weslleyalmeida6@gmail.com>
2023-10-11 20:13:10 -0300

    init

---
+++ src/main/java/br/com/wasystems/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,63 @@
+package br.com.wasystems.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.wasystems.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+        var servletPath = request.getServletPath();
+
+        if(!servletPath.startsWith("/tasks/")) {
+          filterChain.doFilter(request, response);
+          return;
+        }
+
+        var authorization = request.getHeader("Authorization");
+        
+        var authEncoded = authorization.substring("Basic".length()).trim();
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+        var user_password = new String(authDecode);
+
+        String[] credentials = user_password.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
+        var user = this.userRepository.findByUsername(username);
+
+        if (user == null) {
+          response.sendError(401);
+          return;
+        }
+
+        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword().toCharArray());
+        if(passwordVerify.verified) {
+          request.setAttribute("idUser", user.getId());
+          filterChain.doFilter(request, response);
+          return;
+        }
+
+        response.sendError(401);
+      }
+}
---
+++ src/main/java/br/com/wasystems/todolist/task/ITaskRepository.java
@@ -0,0 +1,11 @@
+package br.com.wasystems.todolist.task;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+  List<TaskModel> findByIdUser(UUID idUser);
+  TaskModel findByIdAndIdUser(UUID id, UUID idUser);
+}
